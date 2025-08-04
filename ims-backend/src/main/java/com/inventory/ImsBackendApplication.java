336ca0df1114f2a6af03f1675ca03fa775edca5f
Abinaya <abinaya.eswaran@contractors.roche.com>
2025-08-04 14:06:32 +0530

    metrix fix

--- ims-backend/src/main/java/com/inventory/ImsBackendApplication.java
+++ ims-backend/src/main/java/com/inventory/ImsBackendApplication.java
@@ -2,13 +2,19 @@ package com.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = {
+        SystemMetricsAutoConfiguration.class
+    }
+)
 @EnableJpaAuditing
 public class ImsBackendApplication {
 
 	public static void main(String[] args) {
+		System.out.println("=== IMS Backend is starting ===");
 		SpringApplication.run(ImsBackendApplication.class, args);
 	}
 
